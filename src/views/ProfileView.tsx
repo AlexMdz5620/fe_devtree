6721679a35065aa3adc57c356ea6af3694d4966f
Manu Mdz <manuel.mendozac.dev@gmail.com>
2025-07-20 07:35:05 -0600

    panel para administrar perfil

--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -1,7 +1,89 @@
+import { useForm } from "react-hook-form"
+import ErrorMess from "../components/ErrorMess";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { ProfileForm, User } from "../types";
+import { updateProfile } from "../api/DevTreeApi";
+import { toast } from "sonner";
 
 
 export default function ProfileView() {
+  const queryClient = useQueryClient();
+  const data: User = queryClient.getQueryData(['user'])!;
+
+  const { register, handleSubmit, formState: { errors } } = useForm<ProfileForm>({
+    defaultValues: {
+      handle: data.handle,
+      description: data.description,
+    }
+  });
+
+  const updateProfileMutation = useMutation({
+    mutationFn: updateProfile,
+    onError: errors => { toast.error(errors.message); },
+    onSuccess: data => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ['user'] });
+    }
+  })
+
+  const handleUserProfileForm = (formData: ProfileForm) => {
+    updateProfileMutation.mutate(formData);
+  }
+
   return (
-    <div>ProfilView</div>
+    <form
+      className="bg-white p-10 rounded-lg space-y-5"
+      onSubmit={handleSubmit(handleUserProfileForm)}
+    >
+      <legend className="text-2xl text-slate-800 text-center">Editar Información</legend>
+      <div className="grid grid-cols-1 gap-2">
+        <label
+          htmlFor="handle"
+        >Handle:</label>
+        <input
+          type="text"
+          className="border-none bg-slate-100 rounded-lg p-2"
+          placeholder="handle o Nombre de Usuario"
+          {...register('handle', {
+            required: 'El nombre del usuario es obligatorio'
+          })}
+        />
+        {errors.handle && <ErrorMess>{errors.handle.message}</ErrorMess>}
+      </div>
+
+      <div className="grid grid-cols-1 gap-2">
+        <label
+          htmlFor="description"
+        >Descripción:</label>
+        <textarea
+          className="border-none bg-slate-100 rounded-lg p-2"
+          placeholder="Tu Descripción"
+          {...register('description', {
+            required: 'La descripción es obligatorio'
+          })}
+        />
+        {errors.description && <ErrorMess>{errors.description.message}</ErrorMess>}
+      </div>
+
+      <div className="grid grid-cols-1 gap-2">
+        <label
+          htmlFor="handle"
+        >Imagen:</label>
+        <input
+          id="image"
+          type="file"
+          name="handle"
+          className="border-none bg-slate-100 rounded-lg p-2"
+          accept="image/*"
+          onChange={() => { }}
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="bg-cyan-400 p-2 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+        value='Guardar Cambios'
+      />
+    </form>
   )
 }
